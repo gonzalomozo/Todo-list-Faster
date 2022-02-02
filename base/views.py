e2fac03331b348294541496eaa550c828fd3677c
gonzalomozo <gomzape123@gmail.com>
2022-02-01 20:21:14 -0500

    tareas por id

--- base/views.py
+++ base/views.py
@@ -2,8 +2,13 @@ from django.db import models
 from django.db.models.base import ModelState
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
  
 
 class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+class TaskDetail(DetailView):
     model = Task
